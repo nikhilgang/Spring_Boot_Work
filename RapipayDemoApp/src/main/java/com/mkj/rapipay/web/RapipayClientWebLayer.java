c1a1b09ea2032a8bc95c7b86177a900b4d8302c2
unknown <nikhilgang>
2021-11-15 16:53:16 +0530

    Assignment completed

--- RapipayDemoApp/src/main/java/com/mkj/rapipay/web/RapipayClientWebLayer.java
+++ RapipayDemoApp/src/main/java/com/mkj/rapipay/web/RapipayClientWebLayer.java
@@ -1,7 +1,11 @@
 package com.mkj.rapipay.web;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.mkj.rapipay.service.IRapipayClientService;
@@ -12,7 +16,7 @@ public class RapipayClientWebLayer {
 
 	@Autowired
 	private IRapipayClientService rapipayServiceImpl;
-	
+
 	public RapipayClientWebLayer() {
 		super();
 		// TODO Auto-generated constructor stub
@@ -20,10 +24,19 @@ public class RapipayClientWebLayer {
 	}
 
 	@RequestMapping("/hello")
-	public String helloEndPoint()
-	{
+	public String helloEndPoint() {
 		return "Rapipay client Hello";
 	}
-	
-	
+
+	@RequestMapping("/sum/{num1}/{num2}")
+	public int getSum(@PathVariable(name = "num1") Integer a, @PathVariable(name = "num2") Integer b) {
+
+		return a + b;
+	}
+
+	@RequestMapping("/addition")
+	public int getaddition(@RequestParam int num1,@RequestParam int num2) {
+		return num1+num2;
+	}
+
 }
